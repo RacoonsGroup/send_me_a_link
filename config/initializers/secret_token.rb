5a545369415927a020ce2fea935e6f57661def9a
Marat Islamov <pitabrus@gmail.com>
2012-12-26 22:33:58 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SendMeALink::Application.config.secret_token = 'bbbf1c27c08e7c7ae02e51bda10f7584cab6a1e8b953d3a1be6628de9b705c5a1038a2d515c4589d2dd79befa10baa530723765c3e149a7fdc6ed745a540142e'
