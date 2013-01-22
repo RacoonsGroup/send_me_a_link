965b28b1a7ae7e7a411fc4ba8361ac5e6d047513
Kirill Zonov <graffzon@gmail.com>
2013-01-22 15:55:41 +0400

    renamed slug to login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < ApplicationController
 
   def new
-  	@group = Group.new
+    @group = Group.new
   end
 
   def create
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,10 +2,10 @@ class Group
   include Mongoid::Document
 
   field :name
-  field :slug
+  field :login
   field :approved_emails,     :type => Array
   field :not_approved_emails, :type => Array
 
-  validates_presence_of :name, :slug
-  validates_uniqueness_of :name, :slug
+  validates_presence_of :name, :login
+  validates_uniqueness_of :name, :login
 end
